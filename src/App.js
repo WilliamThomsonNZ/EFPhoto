d1c9e9c0b00704fbf1f7d10e38e5b49df912a5a7
William Thomson <william__thomson@outlook.com>
2020-10-13 19:37:27 +1300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import "./App.css";
 
 function App() {
+  const [background, setBackground] = useState(
+    "https://source.unsplash.com/random/1600x900"
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div
+      className="background"
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <header>
+        <a href="https://instagram.com">
+          <FontAwesomeIcon icon={faCoffee} />
         </a>
+        <h1 className="title">John Doe Photography</h1>
+        <div>Menu</div>
       </header>
+      <main></main>
     </div>
   );
 }
